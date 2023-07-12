54f12c6356c5d9dea556f782d08a41d4a8c8ec62
gmkemcha <gmkemcha@gmail.com>
2023-07-12 17:39:45 +0000

    we modified our build file

--- maven.sh
+++ maven.sh
@@ -2,3 +2,5 @@
 echo 'getting ready to engage in a full time/E2E Automation'
 echo 'I am sure this is going to be interesting'
 echo 'All the same we are raedy for it'
+echo 'we are used to it'
+echo 'surely, this should work as planned'
